4ddee1c6508e2633792e37cce4cf2343d68e39ca
Maggie Liu <mliu0805@gmail.com>
2017-05-02 20:04:43 -0400

    Create Board class

--- snake/board.js
+++ snake/board.js
@@ -1,24 +1,31 @@
 const Snake = require('./snake');
 
 class Board {
-  constructor(size) {
+  constructor(length) {
     this.snake = new Snake(this);
-    this.size = size;
+    this.length = length;
   }
 
   setup() {
     const grid = [];
-    for (let i=0; i < this.size; i++) {
+    for (let i=0; i < this.length; i++) {
       let row = [];
-      for (let j=0; j < this.size; j++) {
+      for (let j=0; j < this.length; j++) {
         row.push(" ");
       }
       grid.push(row);
     }
+    return grid;
   }
 
   render() {
-    
+    const grid = this.setup();
+    this.snake.segments.forEach((segment) => {
+      const x = segment[0];
+      const y = segment[1];
+      grid[x][y] = "S";
+    });
+
   }
 
 }
